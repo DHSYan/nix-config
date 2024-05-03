bbe06d97ec4acd990d94311952ca3e93590bf08c
DHSYan <sean.yan@student.unsw.edu.au>
2024-05-03 15:08:48 +1000

    Added back the caps as esc option

--- configuration.nix
+++ configuration.nix
@@ -51,7 +51,7 @@
   # Configure keymap in X11
   services.xserver.xkb.layout = "us";
   services.xserver.xkb.variant = "dvorak";
-  # services.xserver.xkb.options = "caps:escape";
+  services.xserver.xkb.options = "caps:escape";
 
   services.printing.enable = true; 
 
