394d92bbc8a0aa2df7fea457314040f81d7088ed
Ding-Han Sean Yan <sean@dhsy.me>
2024-10-19 13:55:03 -0700

    moab-dev: hostname correction

--- hosts/moab/default.nix
+++ hosts/moab/default.nix
@@ -17,7 +17,7 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  networking.hostName = "nixos"; 
+  networking.hostName = "moab"; 
   networking.wireless =  {
       enable = true;  # Enables wireless support via wpa_supplicant.
       userControlled.enable = true;
