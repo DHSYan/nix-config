4ecb0943e888a8aafbd2891faa19af1f95beaa56
Ding-Han Sean Yan <sean@dhsy.me>
2024-12-16 01:15:08 -0800

    feat: utilizing flake-util
    devshell now portable to all systems, but not sure if the other stuff in
    the output attrs are affected, pretty sure they are

--- flake.nix
+++ flake.nix
@@ -6,6 +6,7 @@
   # These will get passed as arguments into the outputs function below
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
 
     home-manager = {
       url = "github:nix-community/home-manager";
@@ -34,71 +35,76 @@
       nixpkgs,
       home-manager,
       nixos-hardware,
+      flake-utils,
       ...
     }@inputs:
-    let
-      system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
-    in
-    {
-      nixosConfigurations = {
-        framework = nixpkgs.lib.nixosSystem {
-          inherit system;
-          specialArgs = {
-            inherit inputs;
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        nixosConfigurations = {
+          framework = nixpkgs.lib.nixosSystem {
             inherit system;
+            specialArgs = {
+              inherit inputs;
+              inherit system;
+            };
+            modules = [
+              ./hosts/framework
+              nixos-hardware.nixosModules.framework-11th-gen-intel
+            ];
           };
-          modules = [
-            ./hosts/framework
-            nixos-hardware.nixosModules.framework-11th-gen-intel
-          ];
-        };
-        pc = nixpkgs.lib.nixosSystem {
-          inherit system;
-          specialArgs = {
-            inherit inputs;
+          pc = nixpkgs.lib.nixosSystem {
             inherit system;
+            specialArgs = {
+              inherit inputs;
+              inherit system;
+            };
+            modules = [
+              ./hosts/asus-rog-z790i
+            ];
           };
-          modules = [
-            ./hosts/asus-rog-z790i
-          ];
-        };
 
-        moab = nixpkgs.lib.nixosSystem {
-          inherit system;
-          specialArgs = {
-            inherit inputs;
+          moab = nixpkgs.lib.nixosSystem {
             inherit system;
+            specialArgs = {
+              inherit inputs;
+              inherit system;
+            };
+            modules = [
+              ./hosts/moab
+              nixos-hardware.nixosModules.framework-13-7040-amd
+            ];
           };
-          modules = [
-            ./hosts/moab
-            nixos-hardware.nixosModules.framework-13-7040-amd
-          ];
         };
-      };
 
-      homeConfigurations.tzen = home-manager.lib.homeManagerConfiguration {
-        inherit pkgs;
+        homeConfigurations.tzen = home-manager.lib.homeManagerConfiguration {
+          inherit pkgs;
 
-        modules = [ ./home ];
+          modules = [ ./home ];
 
-      };
+        };
 
-      devShells.${system}.default = pkgs.mkShell {
-        packages = with pkgs; [
-          nixfmt-rfc-style
-          nixd
-        ];
+        devShells = {
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              nixfmt-rfc-style
+              nixd
+            ];
+            name = "nix-dev-shell";
 
+            # inputsFrom = with pkgs; [
+            #   nixfmt-rfc-style
+            # ];
 
-        # inputsFrom = with pkgs; [
-        #   nixfmt-rfc-style
-        # ];
+            shellHook = ''
+              zsh
+            '';
 
-        shellHook = ''
-          zsh
-        '';
-
-      };
-    };
+          };
+        };
+      }
+    );
 }
