23807d89ae427c77536d69d2a623941c782873a4
Ding-Han Sean Yan <sean@dhsy.me>
2025-02-21 19:18:08 -0800

    feat(maccas): remove karabiner elements in the package as it doesnt' work when installed through here

--- hosts/maccas/default.nix
+++ hosts/maccas/default.nix
@@ -57,7 +57,7 @@
             #whatsapp-for-mac
             nodejs_22
             alacritty
-            karabiner-elements
+            # karabiner-elements
             tmux
 # ghostty
         ];
