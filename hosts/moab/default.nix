7091c7bd365ee04f255c12e4783c089c761d8c05
Ding-Han Sean Yan <sean@dhsy.me>
2025-03-17 10:03:19 -0700

    feat(moab): tlp is working

--- hosts/moab/default.nix
+++ hosts/moab/default.nix
@@ -41,7 +41,7 @@
          	CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
 
          	CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
-         	CPU_ENERGY_PERF_POLICY_ON_BAT = "default";
+         	CPU_ENERGY_PERF_POLICY_ON_BAT = "powersave";
 
          	CPU_MIN_PERF_ON_AC = 0;
          	CPU_MAX_PERF_ON_AC = 100;
@@ -49,10 +49,10 @@
          	CPU_MAX_PERF_ON_BAT = 30;
 
          	START_CHARGE_THRESH_BAT0 = 60;
-         	STOP_CHARGE_THRESH_BAT0 = 100;
+         	STOP_CHARGE_THRESH_BAT0 = 70;
 
             START_CHARGE_THRESH_BAT1 = 60;
-            STOP_CHARGE_THRESH_BAT1 = 100;
+            STOP_CHARGE_THRESH_BAT1 = 70;
 
 
          };
