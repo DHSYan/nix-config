82315ea13e5ea1d13900086220526dba099a2cc4
Ding-Han Sean Yan <sean@dhsy.me>
2025-04-01 12:08:52 -0700

    feat(nixsettings): added automatic gc collection timer

--- modules/nixsettings.nix
+++ modules/nixsettings.nix
@@ -3,4 +3,11 @@
     nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
     nix.nixPath = [ "nixpkgs=${inputs.nixpkgs}" ];
+
+    nix.gc = { 
+        automatic = true;
+        randomizedDelaySec = "14m";
+        options = "--delete-older-than 10d"; 
+        dates = "monthly";
+    };
 }
