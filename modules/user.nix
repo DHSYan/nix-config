049e21dca413eb27f09196c03b1356c4ea84a235
Ding-Han Sean Yan <sean@dhsy.me>
2025-08-01 15:12:32 -0700

    feat(mod): docker permission

--- modules/user.nix
+++ modules/user.nix
@@ -3,7 +3,7 @@
   users.users.tzen = {
     isNormalUser = true;
     shell = pkgs.zsh;
-    extraGroups = [ "wheel" "libvirtd" ]; # Enable ‘sudo’ for the user.
+    extraGroups = [ "wheel" "libvirtd" "docker" ]; # Enable ‘sudo’ for the user.
     packages = with pkgs; [
     ];
   };
