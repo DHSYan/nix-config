3fde27dbd04020972f96ee5d3e9075a46d29f713
Ding-Han Sean Yan <sean@dhsy.me>
2025-09-17 16:22:18 -0700

    feat(homelab): add other yubikey to server

--- hosts/pivpn/default.nix
+++ hosts/pivpn/default.nix
@@ -30,6 +30,8 @@
   users.users.root.openssh.authorizedKeys.keys = [
     # change this to your ssh key
     "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIMoVAIrI4nMtW06w18N7W8V86PQnD/16xgTY2KOT/a5rAAAADHNzaDp5dWJpa2V5NQ== ssh:yubikey5"
+
+    "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAINlSFY1nzD/IDexKoe5rta1ghxofI32jhrjm5IIt4K1tAAAADXNzaDp5dWJpa2V5NWM= ssh:yubikey5c"
   ];
 
   system.stateVersion = "24.05";
