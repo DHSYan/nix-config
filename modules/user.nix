e84e06ee224e61ca5caa29ddcdd5dff584b2d868
Ding-Han Sean Yan <sean@dhsy.me>
2025-11-19 13:45:26 -0800

    feat(user): Add new user group of uinput

--- modules/user.nix
+++ modules/user.nix
@@ -8,9 +8,8 @@
       "wheel"
       "libvirtd"
       "docker"
-    ]; # Enable ‘sudo’ for the user.
-    # packages = with pkgs; [
-    # ];
+      "uinput"
+    ]; 
 
     openssh.authorizedKeys.keys = [
       "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIMoVAIrI4nMtW06w18N7W8V86PQnD/16xgTY2KOT/a5rAAAADHNzaDp5dWJpa2V5NQ== ssh:yubikey5"
