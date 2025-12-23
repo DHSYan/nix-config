1b2ae010cc9f40bf336566db47147ab382023fc0
DH Sean Yan <sean@dhsy.dev>
2025-12-24 00:31:43 +0800

    feat(immich): Move filesystem mounting logic to host specific file

--- hosts/dayone/default.nix
+++ hosts/dayone/default.nix
@@ -41,5 +41,35 @@
   networking.interfaces.eth0.wakeOnLan.enable = true;
 
 
+  fileSystems."/mnt/immich" = {
+    device = "/dev/disk/by-uuid/8a74b761-21ba-4744-ad75-1d17bb4bb6dc";
+    fsType = "ext4";
+    options = [
+      # If you don't have this options attribute, it'll default to "defaults"
+      # boot options for fstab. Search up fstab mount options you can use
+      #      "users" # Allows any user to mount and unmount
+      "nofail" # Prevent system from failing if this drive doesn't mount
+      # "umask=002"
+      # "uid=995"
+      # "gid=993"
+      # "allow_other"
+    ];
+  };
+
+  fileSystems."/mnt/files" = {
+    device = "/dev/disk/by-uuid/b18348d6-92ea-47c7-bb5b-8f342e26996a";
+    fsType = "ext4";
+    options = [
+      # If you don't have this options attribute, it'll default to "defaults"
+      # boot options for fstab. Search up fstab mount options you can use
+      #      "users" # Allows any user to mount and unmount
+      "nofail" # Prevent system from failing if this drive doesn't mount
+      # "umask=002"
+      # "uid=995"
+      # "gid=993"
+      # "allow_other"
+    ];
+  };
+
   system.stateVersion = "24.05";
 }
