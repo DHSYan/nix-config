cb11f7e8ba925db6812bf83def4846cb697648a4
DH Sean Yan <sean@dhsy.dev>
2026-01-23 17:01:25 -0800

    feat(networking): remove ubcsecure
    Signed-off-by: DH Sean Yan <sean@dhsy.dev>

--- modules/networking.nix
+++ modules/networking.nix
@@ -26,14 +26,14 @@
       "Hommy" = {
         pskRaw = "ext:perry_psk";
       };
-      "ubcsecure" = {
-        auth = ''
-          key_mgmt=WPA-EAP
-          eap=PEAP
-          identity="dhsyan@ubc.ca"
-          password=ext:eduroam_psk
-        '';
-      };
+      # "ubcsecure" = {
+      #   auth = ''
+      #     key_mgmt=WPA-EAP
+      #     eap=PEAP
+      #     identity="dhsyan@ubc.ca"
+      #     password=ext:eduroam_psk
+      #   '';
+      # };
       "TracyIPhone15" = {
         pskRaw = "ext:momphone_psk";
       };
